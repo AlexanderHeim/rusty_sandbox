d7a26fe4146affac6296ab3c510ffcf13fbb5771
alexanderheim <alexanderheimat@gmail.com>
2020-07-01 23:38:04 +0200

    fixed heat bug

--- src/particle_plane.rs
+++ src/particle_plane.rs
@@ -16,17 +16,20 @@ impl ParticlePlane {
     }
 
     pub fn update(&mut self) {
-        let mut heat_count: u128 = 0;
         for x in 0..GRID_SLOT_AMOUNT.0 as usize {
             for y in 0..GRID_SLOT_AMOUNT.1 as usize {
                 if self.grid[x][y].is_some() {
                     update_specific_particle(self, x, y);
                     transfer_heat(self, x, y);
-                    heat_count += self.grid[x][y].unwrap().temp as u128;
                 }
-                if let Some(particle) = &self.grid[x][y] {
-                    if particle.updateable {
-                        match particle.ptype {
+                
+            }
+        }
+        for x in 0..GRID_SLOT_AMOUNT.0 as usize {
+            for y in 0..GRID_SLOT_AMOUNT.1 as usize {
+                if self.grid[x][y].is_some() {
+                    if self.grid[x][y].unwrap().updateable {
+                        match self.grid[x][y].unwrap().ptype {
                             ParticleType::Sand => move_powder(self, x, y),
                             ParticleType::MoltenGlass => move_liquid(self, x, y),
                             ParticleType::Glass => move_solid(self, x, y),
@@ -37,11 +40,12 @@ impl ParticlePlane {
                             ParticleType::Lava => move_liquid(self, x, y),
                             ParticleType::VaporisedSilicon => move_gas(self, x, y),
                         }
+                        
                     }
                 }
+                
             }
         }
-        println!("{}", heat_count);
     }
 
     pub fn reset_updateable(&mut self) {
